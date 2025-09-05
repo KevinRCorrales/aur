f6f640f304dcd1bdf9ee6a29f62f4bdc6c0a34f9
Kevin <kevin.ramirezcorr@campusucc.edu.co>
2025-09-05 19:06:50 +0000

    Correcion de sintaxis en git

--- añadir.sh
+++ añadir.sh
@@ -2,7 +2,7 @@
 
 repo="$1"
 
-git remote add "$repo" https://aur.archlinux.org/"$repo"
+git remote add "$repo" https://aur.archlinux.org/"$repo".git
 git fetch "$repo"
 git checkout -b "$repo" "$repo"/master
 git checkout main
