3fdc90fcb2887e0febdf921103b05bd9f8291c02
Kevin <kevin.ramirezcorr@campusucc.edu.co>
2025-09-05 19:19:08 +0000

    archivo para añadir remotes

---
+++ addremote.py
@@ -0,0 +1,9 @@
+import subprocess as sb
+
+ramas = sb.check_output(["git", "branch"], text=True)
+for rama in ramas.split("\n"):
+    rama = rama[2:]  # Quitar los dos espacios que deja git branch
+    if rama == "main" or rama == "":
+        pass
+    else:
+        sb.run(["git", "remote", "add", rama, f"https://aur.archlinux.org/{rama}.gio"])
